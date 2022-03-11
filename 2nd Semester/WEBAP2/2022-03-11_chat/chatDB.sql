ff9e571dd22aeb24fe3c3a4359df0dee875d3f0d
leonardomonticelli <leonardomonticelli19@gmail.com>
2022-03-11 10:11:29 +0100

    update

---
+++ 2nd Semester/WEBAP2/2022-03-11_chat/chatDB.sql
@@ -0,0 +1,18 @@
+-- drop database chat;
+create database chat;
+use chat;
+
+create table users(
+    userID int not null auto-increment,
+    userName varchar(20),
+    primary key (userID)
+);
+
+create table messages(
+    msgID int not null auto_increment,
+    msgTime time,
+    msgText varchar(100),
+    fromUser int not null,
+    primary key (msgID),
+    foreign key (fromUser) references users(userID)
+)
---
+++ 2nd Semester/WEBAP2/2022-03-11_chat/chatEnter.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Chat Enter</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1>
+        Welcome to our chatroom entry point:
+    </h1>
+
+    <form method="POST" action="chatRoom.php">
+        Please type-in your username:
+        <input type="" name="userName">
+        <input type="submit" value="Enter">
+    </form>
+</body>
+</html>
